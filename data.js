d097799afabd9d0e302a5e6a82fdc12fcae3b034
TrangCQ <trangcq@rikkeisoft.com>
2022-03-10 18:52:38 +0700

    demo connect graphql

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = {
+  books: [
+    {
+      id: 1,
+      title: 'The Awakening',
+      author: {
+        name: 'Edison',
+        age: 27
+      },
+    },
+    {
+      id: 2,
+      title: 'City of Glass',
+      author: {
+        name: 'Andy',
+        age: 29
+      }
+    },
+    {
+      id: 3,
+      title: 'Secret of Naoko',
+      author: {
+        name: 'Keigo',
+        age: 59
+      }
+    },
+  ],
+  authors: [
+    {
+      name: 'Edison',
+      age: 27
+    },
+    {
+      name: 'Andy',
+      age: 29,
+    }
+  ]
+}
+
+module.exports = data
